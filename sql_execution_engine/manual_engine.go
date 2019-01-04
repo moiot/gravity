97ae49a36110196506a33e7d53f1c7206db710ef
Hu Ming <humings@gmail.com>
2019-01-04 10:53:07 +0800

    add table name based on target table in manual sql engine (#15)
    * use golang template in sql template

--- sql_execution_engine/manual_engine.go
+++ sql_execution_engine/manual_engine.go
@@ -2,16 +2,22 @@ package sql_execution_engine
 
 import (
 	"database/sql"
-
 	"github.com/juju/errors"
 	log "github.com/sirupsen/logrus"
+	"strings"
+	"text/template"
 
 	"github.com/moiot/gravity/pkg/core"
 	"github.com/moiot/gravity/schema_store"
 )
 
+type sqlTemplateContext struct {
+	TargetTable *schema_store.Table
+	Msg *core.Msg
+}
+
 type manualSQLEngine struct {
-	sqlTemplate       string
+	sqlTemplate       *template.Template
 	sqlArgsExpression []string
 	db                *sql.DB
 }
@@ -36,18 +42,35 @@ func (engine *manualSQLEngine) Execute(msgBatch []*core.Msg, tableDef *schema_st
 		sqlArgs[i] = msg.DmlMsg.Data[field]
 	}
 
-	_, err := engine.db.Exec(engine.sqlTemplate, sqlArgs...)
+	templateContext := sqlTemplateContext{
+		TargetTable: tableDef,
+		Msg: msg,
+	}
+
+	stringBuilder := strings.Builder{}
+	if err := engine.sqlTemplate.Execute(&stringBuilder, templateContext); err != nil {
+		return errors.Trace(err)
+	}
+
+	query := stringBuilder.String()
+	_, err := engine.db.Exec(query, sqlArgs...)
 	if err != nil {
-		log.Errorf("[manualSQLEngine] error sql: %s, sqlArgs: %v, err: %v", engine.sqlTemplate, sqlArgs, err.Error())
+		log.Errorf("[manualSQLEngine] error sql: %s, sqlArgs: %v, err: %v", query, sqlArgs, err.Error())
 		return errors.Trace(err)
 	}
 	return nil
 }
 
+
 func NewManualSQLEngine(db *sql.DB, config MySQLExecutionEngineConfig) SQlExecutionEngine {
+	t, err := template.New("sqlTemplate").Parse(config.SQLTemplate)
+	if err != nil {
+		log.Fatalf("[manualSQLEngine] failed to parse template: %v", config.SQLTemplate)
+	}
+
 	engine := manualSQLEngine{
 		db:                db,
-		sqlTemplate:       config.SQLTemplate,
+		sqlTemplate:       t,
 		sqlArgsExpression: config.SQLArgExpr,
 	}
 	return &engine
