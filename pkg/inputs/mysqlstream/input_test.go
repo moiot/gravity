1ac6e44686bcede95401f23d9e548bfb2e81e47f
renhongdi <ryan.hd.ren@gmail.com>
2019-01-15 10:37:04 +0800

    fix(MySQLBinlogPosition): fix tag

---
+++ pkg/inputs/mysqlstream/input_test.go
@@ -0,0 +1,34 @@
+package mysqlstream
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_mysqlInputPlugin_Configure(t *testing.T) {
+	r := require.New(t)
+	raw := `{
+      "ignore-bidirectional-data": true,
+      "source": {
+        "host": "localhost"
+      },
+      "start-position": {
+        "binlog-gtid": "gtid"
+      }
+    }`
+
+	var m map[string]interface{}
+	err := json.Unmarshal([]byte(raw), &m)
+	r.NoError(err)
+
+	p := mysqlInputPlugin{}
+	err = p.Configure("123", m)
+	r.NoError(err)
+
+	cfg := p.cfg
+	r.Equal(true, cfg.IgnoreBiDirectionalData)
+	r.Equal("localhost", cfg.Source.Host)
+	r.Equal("gtid", cfg.StartPosition.BinlogGTID)
+}
