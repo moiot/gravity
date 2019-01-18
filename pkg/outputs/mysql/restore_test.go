0f6ba01d275cf1a38a91d1f864d9c89ad637c62d
Ryan-Git <ryan.hd.ren@gmail.com>
2019-01-18 17:05:20 +0800

    Fix ddl (#55)
    * fix(output.mysql): handle multi line ddl.

--- pkg/outputs/mysql/restore_test.go
+++ pkg/outputs/mysql/restore_test.go
@@ -11,29 +11,83 @@ import (
 )
 
 func TestRestoreCreateTblStmt(t *testing.T) {
-	ddl := "CREATE TABLE `t` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`fl` float(5,1) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"
-
-	p := parser.New()
-	stmt, err := p.ParseOneStmt(ddl, "", "")
-	if err != nil {
-		t.Fatalf("sql parser error: %v", err.Error())
+	cases := []struct {
+		name        string
+		ifNotExists bool
+		input       string
+		expect      string
+	}{
+		{
+			name:        "add ifNotExists",
+			ifNotExists: true,
+			input:       "CREATE TABLE `t` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`fl` float(5,1) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4",
+			expect:      "CREATE TABLE IF NOT EXISTS `t` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`fl` float(5,1) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4",
+		},
+		{
+			name: "multi line",
+			input: `CREATE TABLE t5 (
+  id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  thirdparty tinyint(2) NOT NULL DEFAULT '0' COMMENT '第三方编号',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='第三方调用记录'`,
+			expect: "CREATE TABLE `t5` (" + `
+  id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  thirdparty tinyint(2) NOT NULL DEFAULT '0' COMMENT '第三方编号',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='第三方调用记录'`,
+		},
 	}
-	tableStmt := stmt.(*ast.CreateTableStmt)
-	tableStmt.IfNotExists = true
-	ret := RestoreCreateTblStmt(tableStmt)
-	require.Equal(t, "CREATE TABLE IF NOT EXISTS `t` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`fl` float(5,1) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", ret)
+
+	r := require.New(t)
+	p := parser.New()
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			stmt, err := p.ParseOneStmt(c.input, "", "")
+			r.NoError(err)
+			tableStmt := stmt.(*ast.CreateTableStmt)
+			tableStmt.IfNotExists = c.ifNotExists
+			ret := RestoreCreateTblStmt(tableStmt)
+			r.Equal(c.expect, ret)
+		})
+	}
 }
 
 func TestRestoreAlterTblStmt(t *testing.T) {
-	ddl := "alter table test.t add column ii int(10);"
-
-	p := parser.New()
-	stmt, err := p.ParseOneStmt(ddl, "", "")
-	if err != nil {
-		t.Fatalf("sql parser error: %v", err.Error())
+	cases := []struct {
+		name      string
+		tableName string
+		input     string
+		expect    string
+	}{
+		{
+			name:      "rewrite table name",
+			tableName: "t2",
+			input:     "alter table test.t add column ii int(10)",
+			expect:    "ALTER TABLE `test`.`t2` add column ii int(10)",
+		},
+		{
+			name: "multi line",
+			input: `alter table test.t 
+add column ii int(10)`,
+			expect: "ALTER TABLE `test`.`t` " + `
+add column ii int(10)`,
+		},
 	}
-	alterTableStmt := stmt.(*ast.AlterTableStmt)
-	alterTableStmt.Table.Name.O = "t2"
-	ret := RestoreAlterTblStmt(alterTableStmt)
-	require.Equal(t, "ALTER TABLE `test`.`t2` add column ii int(10);", ret)
+
+	r := require.New(t)
+	p := parser.New()
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			stmt, err := p.ParseOneStmt(c.input, "", "")
+			r.NoError(err)
+			alterTableStmt := stmt.(*ast.AlterTableStmt)
+			if c.tableName != "" {
+				alterTableStmt.Table.Name.O = c.tableName
+			}
+			ret := RestoreAlterTblStmt(alterTableStmt)
+			r.Equal(c.expect, ret)
+		})
+	}
 }
