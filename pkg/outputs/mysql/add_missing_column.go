0a7cbbe2b546d730f8742c9e06a5bebcd608a0bf
huming <humings@gmail.com>
2019-01-21 12:48:32 +0800

    fix add missing column

--- pkg/outputs/mysql/add_missing_column.go
+++ pkg/outputs/mysql/add_missing_column.go
@@ -5,6 +5,8 @@ import (
 	"github.com/moiot/gravity/pkg/schema_store"
 )
 
+// AddMissingColumn add data to msg if target table has extra columns.
+// It is considered safe since this casting does not lose information.
 func AddMissingColumn(msg *core.Msg, targetTableDef *schema_store.Table) (bool, error) {
 	if msg.DmlMsg == nil || targetTableDef == nil {
 		return true, nil
@@ -16,8 +18,12 @@ func AddMissingColumn(msg *core.Msg, targetTableDef *schema_store.Table) (bool, 
 
 		name := column.Name
 		_, ok := msg.DmlMsg.Data[name]
-		if !ok && !column.DefaultVal.IsNull {
-			msg.DmlMsg.Data[name] = targetColumns[i].DefaultVal.ValueString
+		if !ok {
+			if column.DefaultVal.IsNull {
+				msg.DmlMsg.Data[name] = nil
+			} else {
+				msg.DmlMsg.Data[name] = targetColumns[i].DefaultVal.ValueString
+			}
 		}
 	}
 	return true, nil
