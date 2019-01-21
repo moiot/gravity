ad4b8d4d23be12f91804ca149d5e2ad88ceff5a8
Hu Ming <humings@gmail.com>
2019-01-21 12:54:01 +0800

    fix add missing column (#61)

--- pkg/outputs/mysql/add_missing_column_test.go
+++ pkg/outputs/mysql/add_missing_column_test.go
@@ -1,40 +1,69 @@
 package mysql
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/moiot/gravity/pkg/core"
 
 	"github.com/moiot/gravity/pkg/schema_store"
 )
 
-var _ = Describe("add_missing_column", func() {
-	Context("when data does not have a field in target column", func() {
-		It("adds the missing column with default column data", func() {
-			msg := &core.Msg{
-				DmlMsg: &core.DMLMsg{
-					Data:      make(map[string]interface{}),
-					Operation: core.Insert,
-				},
-			}
+func TestAddMissingColumn(t *testing.T) {
+	r := require.New(t)
 
-			t := &schema_store.Table{
-				Columns: []schema_store.Column{
-					{
-						Name:       "a",
-						IsNullable: false,
-						DefaultVal: schema_store.ColumnValueString{
-							ValueString: "123",
-							IsNull:      false,
-						},
+	t.Run("when the missing column have default value NOT NULL", func(tt *testing.T) {
+		msg := &core.Msg{
+			DmlMsg: &core.DMLMsg{
+				Data:      make(map[string]interface{}),
+				Operation: core.Insert,
+			},
+		}
+
+		t := &schema_store.Table{
+			Columns: []schema_store.Column{
+				{
+					Name:       "a",
+					IsNullable: false,
+					DefaultVal: schema_store.ColumnValueString{
+						ValueString: "123",
+						IsNull:      false,
 					},
 				},
-			}
-			b, err := AddMissingColumn(msg, t)
-			Expect(b).To(BeTrue())
-			Expect(err).To(BeNil())
-			Expect(msg.DmlMsg.Data["a"]).To(Equal("123"))
-		})
+			},
+		}
+		b, err := AddMissingColumn(msg, t)
+		r.True(b)
+		r.NoError(err)
+		r.Equal("123", msg.DmlMsg.Data["a"])
 	})
-})
+
+	t.Run("when the missing column have default value of NULL", func(tt *testing.T) {
+		msg := &core.Msg{
+			DmlMsg: &core.DMLMsg{
+				Data:      make(map[string]interface{}),
+				Operation: core.Insert,
+			},
+		}
+
+		t := &schema_store.Table{
+			Columns: []schema_store.Column{
+				{
+					Name:       "a",
+					IsNullable: true,
+					DefaultVal: schema_store.ColumnValueString{
+						ValueString: "",
+						IsNull:      true,
+					},
+				},
+			},
+		}
+		b, err := AddMissingColumn(msg, t)
+		r.True(b)
+		r.NoError(err)
+		v, ok := msg.DmlMsg.Data["a"]
+		r.True(ok)
+		r.Nil(v)
+	})
+}
