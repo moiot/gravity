bd507f924ad8e397c9bfdac2ce348ef62edd4093
renhongdi <ryan.hd.ren@gmail.com>
2019-01-25 18:25:40 +0800

    fix test

--- integration_test/util_test.go
+++ integration_test/util_test.go
@@ -3,7 +3,13 @@ package integration_test
 import "encoding/json"
 
 func struct2Map(i interface{}) (ret map[string]interface{}) {
-	t, _ := json.Marshal(i)
-	_ = json.Unmarshal(t, &ret)
+	t, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(t, &ret)
+	if err != nil {
+		panic(err)
+	}
 	return
 }
