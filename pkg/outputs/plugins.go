ace637bb1f2129f894d3238d8ed2dc3b324a832d
huming <humings@gmail.com>
2019-03-15 10:34:06 +0800

    add stdout output to help debug issue

--- pkg/outputs/plugins.go
+++ pkg/outputs/plugins.go
@@ -3,9 +3,11 @@ package outputs
 import (
 	"github.com/moiot/gravity/pkg/outputs/async_kafka"
 	"github.com/moiot/gravity/pkg/outputs/mysql"
+	"github.com/moiot/gravity/pkg/outputs/stdout"
 )
 
 const (
 	AsyncKafka = async_kafka.Name
 	Mysql      = mysql.Name
+	Stdout     = stdout.Name
 )
