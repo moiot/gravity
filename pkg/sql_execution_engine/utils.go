4db4f936e0d82319dc09f27897446b5537000d3f
renhongdi <ryan.hd.ren@gmail.com>
2019-03-26 10:27:51 +0800

    [skip ci] add comment

--- pkg/sql_execution_engine/utils.go
+++ pkg/sql_execution_engine/utils.go
@@ -169,7 +169,7 @@ func adjustArgs(arg interface{}, column *schema_store.Column) interface{} {
 			return t.Format("2006-01-02 15:04:05.999999999")
 		}
 	}
-	// mysql driver don't know how to handle object id
+	// mysql driver handle object id correctly, it reports Error 1366: Incorrect string value: '\\x99\\x8B\\x1E\\x0B\\x16\\xA8...' for column '_id' at row 1
 	if bid, ok := arg.(bson.ObjectId); ok {
 		return bid.Hex()
 	}
