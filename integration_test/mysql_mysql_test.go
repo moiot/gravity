71d4cb16c05dc0a6e1812ed3fb77ce276f9f7cc2
renhongdi <ryan.hd.ren@gmail.com>
2019-08-25 14:33:04 +0800

    fix test

--- integration_test/mysql_mysql_test.go
+++ integration_test/mysql_mysql_test.go
@@ -1248,7 +1248,7 @@ func TestMySQLDDL(t *testing.T) {
 
 	server.Close()
 
-	_, err = sourceDB.Exec(fmt.Sprintf("select * from `%s`.`tn4`", targetDBName))
+	_, err = sourceDB.Exec(fmt.Sprintf("select * from `%s`.`abc2`", targetDBName))
 	r.NoError(err)
 }
 
