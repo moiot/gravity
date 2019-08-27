058274df3faeaec13ca48369de5b02f6cb41c67a
Ryan-Git <ryan.hd.ren@gmail.com>
2019-08-27 15:39:49 +0800

    fix create table like (#200)

--- integration_test/mysql_mysql_test.go
+++ integration_test/mysql_mysql_test.go
@@ -1224,6 +1224,8 @@ func TestMySQLDDL(t *testing.T) {
 		"create table `abc`(`id` int(11),  PRIMARY KEY (`id`)) ENGINE=InnoDB",
 
 		"drop table tn3, tn4",
+
+		fmt.Sprintf("create table `%s`.`abc2` like `%s`.`abc`", sourceDBName, sourceDBName),
 	}
 
 	for _, ddl := range ddls {
@@ -1245,6 +1247,9 @@ func TestMySQLDDL(t *testing.T) {
 	<-server.Input.Done()
 
 	server.Close()
+
+	_, err = targetDB.Exec(fmt.Sprintf("select * from `%s`.`abc2`", targetDBName))
+	r.NoError(err)
 }
 
 func TestMySQLDDLNoRoute(t *testing.T) {
