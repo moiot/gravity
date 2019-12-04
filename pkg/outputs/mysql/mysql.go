e34e61345c89d00ba83b3069b4b210188e659e26
recall704 <tk657309822@gmail.com>
2019-12-04 11:04:28 +0800

    fix mysql output close panic

--- pkg/outputs/mysql/mysql.go
+++ pkg/outputs/mysql/mysql.go
@@ -131,8 +131,12 @@ func (output *MySQLOutput) Start() error {
 }
 
 func (output *MySQLOutput) Close() {
-	output.db.Close()
-	output.targetSchemaStore.Close()
+	if output.db != nil {
+		output.db.Close()
+	}
+	if output.targetSchemaStore != nil {
+		output.targetSchemaStore.Close()
+	}
 }
 
 func (output *MySQLOutput) GetRouter() core.Router {
